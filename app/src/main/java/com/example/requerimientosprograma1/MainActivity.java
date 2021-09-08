0158db0853748363334016d22393dbc5660bc363
KeylorCalderon <43432543+KeylorCalderon@users.noreply.github.com>
2021-09-07 23:51:39 -0600

    Play/Pause

--- app/src/main/java/com/example/requerimientosprograma1/MainActivity.java
+++ app/src/main/java/com/example/requerimientosprograma1/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 //import android.support.v7.app.AppCompatActivity;
 
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
@@ -14,12 +16,16 @@ import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
+import com.spotify.android.appremote.api.ConnectApi;
+import com.spotify.android.appremote.api.ContentApi;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String CLIENT_ID = "74205026b5424c308f3c1dc486a4be15";
     private static final String REDIRECT_URI = "https://requerimientosproyecto1.com/callback";
     private SpotifyAppRemote mSpotifyAppRemote;
+    //private SpotifyApi spotifyApi;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,16 +47,12 @@ public class MainActivity extends AppCompatActivity {
                     public void onConnected(SpotifyAppRemote spotifyAppRemote) {
                         mSpotifyAppRemote = spotifyAppRemote;
                         Log.d("MainActivity", "Connected! Yay!");
-
-                        // Now you can start interacting with App Remote
                         connected();
 
                     }
 
                     public void onFailure(Throwable throwable) {
                         Log.e("MyActivity", throwable.getMessage(), throwable);
-
-                        // Something went wrong when attempting to connect! Handle errors here
                     }
                 });
     }
@@ -74,5 +76,26 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("MainActivity", track.name + " by " + track.artist.name);
                     }
                 });
+    }
+
+    //BQADjSnnoAgYTs2r18_O_t5VtxAOrFt5L8clXxfn0aEgL_M04gTrHytDU6m8VlXswRtlAuy05D3-6O_fz9FijCROhmFW9heUSEg35D_qCt6ZLb6CyY5691BHHtmDPF30AbAcMQmzaevurL-IoFtMKGPOMQl0sLh8eKS8GbgxtkVsGfiniF9pTx8jSLru5J74kmWOcI_832UzsdmLh-8vHWhRtzNsPGDU
+
+    public void reanudarPausar(View view) {
+        /*Toast.makeText(
+                getApplicationContext(),
+                "HICISTE CLICK EN EL BOTÓN",
+                Toast.LENGTH_LONG).show();*/
+        Button boton = (Button)view;
+        if(boton.getText().equals("Pause")){
+            mSpotifyAppRemote.getPlayerApi().pause();
+            boton.setText("Play");
+        }else{
+            mSpotifyAppRemote.getPlayerApi().resume();
+            boton.setText("Pause");
+        }
+    }
+
+    public void añadirCancion(View view) {
+
     }
 }
